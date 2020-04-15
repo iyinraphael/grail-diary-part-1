99d13936ee014de4022812a1fabd3267309fa185
iyinraphael <iyinraphael@gmail.com>
2020-04-14 23:32:13 -0700

    initial setup

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Iyin Raphael on 4/14/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
