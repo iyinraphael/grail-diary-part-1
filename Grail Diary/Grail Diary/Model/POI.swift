d18915e7ea1dacaa1dbf7a3ddd3d42e43eb55b2b
iyinraphael <iyinraphael@gmail.com>
2020-04-23 00:50:26 -0700

    MVC design pattern

---
+++ Grail Diary/Grail Diary/Model/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  Grail Diary
+//
+//  Created by Iyin Raphael on 4/23/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
---
+++ Grail Diary/Grail Diary/View Controller/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  Grail Diary
+//
+//  Created by Iyin Raphael on 4/23/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
